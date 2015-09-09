36787fd7f92967c7eff8606450d83baa64e68e5f
Victoria <tapia.victoria5070@gmail.com>
2015-09-08 17:17:47 -0700

    added/modified client-side files for chatterbox

---
+++ client/config.js
@@ -0,0 +1,5 @@
+// Put your parse application keys here!
+// $.ajaxPrefilter(function (settings, _, jqXHR) {
+//   jqXHR.setRequestHeader("X-Parse-Application-Id", "wPfavNOxOKTzlXiMq9r226nSo5Vl5aMFfEcvAupE");
+//   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "eCGqXWtDjoZofGzf785KvSODrUrXURbL6JCQqaHj");
+// });
